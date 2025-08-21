3994b629466d3b8e92b37cc270421230c4e35944
wennhmann <info@wennhmann.de>
2025-08-21 12:46:17 +0200

    vector sortet in spawnd thread

--- x_paralell/src/main.rs
+++ x_paralell/src/main.rs
@@ -2,22 +2,31 @@ use std::thread;
 use std::time::Duration;
 
 
-fn doit(){
-    let id = thread::current().id();
-    println!("thread id: {:?}", id);
+
    
-}
+
 fn main() {
-    let t1 = thread::spawn(doit);
-    let t2 = thread::spawn(doit);
-    let t3 = thread::spawn(doit);
-
-    // Simulate some work in the main thread
-    thread::sleep(Duration::from_secs(2));
-    let id = thread::current().id();
-    println!("main thread id: {:?}", id);
-
-    t1.join().unwrap();
-    t2.join().unwrap();
-    t3.join().unwrap();
+    let numbers = vec![1, 2, 3, 4, 5,6, 7, 8, 9, 10,11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let mut odd = vec![];
+    let mut even = vec![];
+    thread::spawn(move||{
+        for n in numbers{
+            if n % 2 == 0 {
+                even.push(n);
+            } else {
+                odd.push(n);
+            }
+        }
+    println!("Odd numbers: {:?}", &odd);
+    println!("Even numbers: {:?}", &even);
+    })
+    .join()
+    .expect("Thread panicked");
+   
+    thread::sleep(Duration::from_secs(1));
+    println!("Main thread finished.");
+    // This is a simple example of using threads to process a vector of numbers.
+    // The main thread creates a new thread to separate odd and even numbers.
+    // The results are printed after the thread completes its execution.
 }
+// This code demonstrates basic thread usage in Rust, including spawning a thread and waiting for it to
