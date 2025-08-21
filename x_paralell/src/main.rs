442235466526827b87c172e89c3bf0c99b22164c
wennhmann <info@wennhmann.de>
2025-08-21 13:18:33 +0200

    borrowing vec in two threads

--- x_paralell/src/main.rs
+++ x_paralell/src/main.rs
@@ -1,17 +1,19 @@
 use std::thread;
-use std::time::Duration;
+//use std::time::Duration;
 
 fn main() {
     let numbers = Vec::from_iter(1..=500);
-    let t1 = thread::spawn(move||{
-        let len =numbers.len();
-        let sum = numbers.iter().sum::<i32>();
-      
-        sum as f64 / len as f64
-     
-        
-    }).join().unwrap();
-    thread::sleep(Duration::from_secs(1));
-    println!("{:?}",t1);
+thread::scope(|s| {
+    s.spawn(|| {
+        println!("Länge: {}", numbers.len()); 
+    }); 
+
+    
+    s.spawn(|| {
+        for n in &numbers { 
+            println!("{n}"); 
+        } 
+    }); 
+});
 }
 
