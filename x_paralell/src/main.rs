de92a5fcd9d56c779cb7a7049548dde6ac95e900
wennhmann <info@wennhmann.de>
2025-08-23 12:23:39 +0200

    riferbar woken up

--- x_paralell/src/main.rs
+++ x_paralell/src/main.rs
@@ -1,28 +1,18 @@
-use std::thread;
-//use std::rc::Rc;
-//use std::time::Duration;
-use std::sync::Arc;
-
+use std::time::Duration;
+use std::thread::{self};
+use std::sync::Mutex;
 fn main() {
-    let mut numbers_a = Arc::new(Vec::from_iter(1..=5));
-    let mut numbers_b = numbers_a.clone();
-
-   //create two threads in a for loop
-    for i in 0..2 {
-        let numbers = if i == 0 { &numbers_a } else { &numbers_b };
-        let numbers = Arc::clone(numbers);
-        
-        thread::spawn(move || {
-            for number in numbers.iter() {
-                println!("Thread {}: {}", i, number);
-                //thread::sleep(Duration::from_millis(100));
-            }
-        });
-    }
-
-    // Wait for threads to finish
-    thread::sleep(std::time::Duration::from_secs(1));
-    println!("All threads completed.");
-
+    let n = Mutex::new(0);
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut guard = n.lock().unwrap();
+                for _ in 0..100 {
+                    *guard += 1;
+                }
+                thread::sleep(Duration::from_secs(1)); // New!
+            });
+        }
+    });
+    assert_eq!(n.into_inner().unwrap(), 1000);
 }
-
