0f0ff269ce06f2b25968dbac8b0a7b7412b295e3
wennhmann <info@wennhmann.de>
2025-08-23 17:42:10 +0200

    mutex in scope

--- x_paralell/src/main.rs
+++ x_paralell/src/main.rs
@@ -1,7 +1,17 @@
 use std::time::Duration;
 use std::thread;
-use std::sync::{Mutex, Arc}; // Arc muss importiert werden
+use std::sync::{Mutex}; // Arc muss importiert werden
 
 fn main() {
-    
+   let n = Mutex::new(0);
+   thread::scope(|s|{
+    for _ in 1..=10{
+        s.spawn(||{
+            let mut guard = n.lock().unwrap();
+            *guard +=10;
+            println!("Thread hat gaerd auf {} gesetzt",  *guard);
+            thread::sleep(Duration::from_millis(50));
+        });
+    }
+   })
 }
